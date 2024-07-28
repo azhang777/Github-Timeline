ef56f309602c4d369828306003c38b71ab9ecff7
Andy <andyzhanggm@gmail.com>
2024-07-28 15:33:48 -0400

    simple setup backend

---
+++ server/src/index.ts
@@ -0,0 +1,37 @@
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+import mongoose from "mongoose";
+import "dotenv/config";
+import router from "./router";
+
+//console.log(process.env);
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(process.env.PORT, () => {
+  console.log(`Server running on http://localhost:${process.env.PORT}`);
+});
+
+const MONGO_URL = `mongodb+srv://${process.env.MY_USERNAME}:${process.env.MY_PASSWORD}@express.mkucxcv.mongodb.net/?retryWrites=true&w=majority&appName=express`;
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+
+mongoose.connection.on("error", (error: Error) => console.log(error));
+
+app.use("/", router());
